6ecf5641519cc7d1e4c56debce7cec10db460564
David <david-bourrel@hotmail.fr>
2023-08-28 11:58:04 +0200

    React Hook "useTranslation problem fixed

--- src/components/modules/AsideProductCategories/index.tsx
+++ src/components/modules/AsideProductCategories/index.tsx
@@ -3,10 +3,43 @@
 import useTranslation from 'next-translate/useTranslation'
 import Heading from '@/components/elements/Heading'
 import AsideProductCategory from './AsideProductCategory'
-import { routes } from './routes'
+import { ProductCategoryEnum } from '@/_types/products'
 
 const AsideProductCategories = () => {
-  const { t } = useTranslation('home')
+  const { t, lang } = useTranslation('home')
+
+  const routes = [
+    {
+      id: 1,
+      href: `/${lang}`,
+      label: t('ourMenu')
+    },
+    {
+      id: 2,
+      href: `/${lang}/products/category/${ProductCategoryEnum.Burger}`,
+      label: t('burgerTitle')
+    },
+    {
+      id: 3,
+      href: `/${lang}/products/category/${ProductCategoryEnum.Side}`,
+      label: t('sideTitle')
+    },
+    {
+      id: 4,
+      href: `/${lang}/products/category/${ProductCategoryEnum.Drink}`,
+      label: t('drinkTitle')
+    },
+    {
+      id: 5,
+      href: `/${lang}/products/category/${ProductCategoryEnum.Dessert}`,
+      label: t('dessertTitle')
+    },
+    {
+      id: 6,
+      href: `/${lang}/products/category/${ProductCategoryEnum.Salad}`,
+      label: t('saladTitle')
+    }
+  ]
 
   return (
     <aside className='hidden lg:mr-8 lg:flex lg:flex-shrink-0 lg:flex-col lg:bg-primary'>
