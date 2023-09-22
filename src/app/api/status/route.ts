4737c2ee51249920dc8756fa27c5aa6a4f27ab71
David <david-bourrel@hotmail.fr>
2023-09-22 15:22:43 +0200

    status route protected by auth0

--- src/app/api/status/route.ts
+++ src/app/api/status/route.ts
@@ -1,10 +1,11 @@
+import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = async (request: NextRequest) => {
+export const GET = withApiAuthRequired(async (request: NextRequest) => {
   return new NextResponse(JSON.stringify({ AVAILABLE: true }), {
     status: 200,
     headers: {
       'Content-Type': 'application/json'
     }
   })
-}
+})
