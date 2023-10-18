17879ba02516190d61b6ac7578616a0051edc9a1
davidbourrel <david-bourrel@hotmail.fr>
2023-10-18 21:14:39 +0200

    dynamic destination on leaflet map done

--- src/components/modules/Location/Map.tsx
+++ src/components/modules/Location/Map.tsx
@@ -1,24 +1,53 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import 'leaflet-defaulticon-compatibility'
-import { mapUrl, position } from './const'
+import {
+  MAP_URL,
+  ESTABLISHMENT_POSITION,
+  PARIS_DEFAULT_POSITION
+} from './const'
 import CustomMarker from './CustomMaker'
 import Address from './Address'
 
 const Map = () => {
+  const [userLocation, setUserLocation] = useState<number[]>(
+    PARIS_DEFAULT_POSITION
+  )
+
+  useEffect(() => {
+    const successFunction = (position: GeolocationPosition) => {
+      const { latitude, longitude } = position?.coords
+      setUserLocation(
+        position?.coords ? [latitude, longitude] : PARIS_DEFAULT_POSITION
+      )
+    }
+
+    const getUserLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successFunction)
+      } else {
+        toast.error('Geolocation is not supported by this browser.')
+      }
+    }
+
+    getUserLocation()
+  }, [])
+
   return (
     <MapContainer
       doubleClickZoom={false}
-      center={position}
-      zoom={15}
+      center={ESTABLISHMENT_POSITION}
+      zoom={7}
       className='relative h-full w-full'
     >
-      <TileLayer url={mapUrl} detectRetina={true} />
-      <CustomMarker position={position} open>
-        <Address />
+      <TileLayer url={MAP_URL} detectRetina={true} />
+      <CustomMarker position={ESTABLISHMENT_POSITION} open>
+        <Address position={userLocation} />
       </CustomMarker>
     </MapContainer>
   )
--- src/components/modules/Location/const.ts
+++ src/components/modules/Location/const.ts
@@ -1,4 +1,4 @@
-export const itineraryUrl = `https://www.google.com/maps/dir/43.2177152,2.3363584/43.6000397,1.4422578/@43.6001646,1.4424641,18z/data=!4m10!4m9!1m6!3m4!1m2!1d1.4440062!2d43.6003963!3s0x12aebc9d44e25bef:0x54d42a66916362fb!4e1!1m0!3e0`
+export const ESTABLISHMENT_POSITION: [number, number] = [45.221387, 1.251147]
+export const PARIS_DEFAULT_POSITION = [48.8635687035092, 2.346345879844896]
 
-export const position: [number, number] = [43.59990664483555, 1.442283655554873]
-export const mapUrl = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`
+export const MAP_URL = `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_KEY}`
