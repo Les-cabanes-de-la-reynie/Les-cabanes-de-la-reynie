5f020c547161cb1f82012e32aad0ca3e71440c77
davidbourrel <david-bourrel@hotmail.fr>
2023-11-06 21:47:17 +0100

    try fix dashboard

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,18 +1,16 @@
+// @ts-nocheck
+
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
+export const prisma: PrismaClient =
+  global.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error']
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
 }
-
-const isBrowser = typeof window !== 'undefined'
-
-export const prisma = (
-  isBrowser
-    ? undefined
-    : globalForPrisma.prisma ??
-      new PrismaClient({
-        log: ['query']
-      })
-) as PrismaClient
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
