772421488e6cd3e3e4a79034861c63bbbe24bded
davidbourrel <david-bourrel@hotmail.fr>
2023-11-06 21:31:19 +0100

    admin dashboard and active day in DayRow component fixed

--- src/components/modules/OpeningHours/Edit/OpeningHoursForm.tsx
+++ src/components/modules/OpeningHours/Edit/OpeningHoursForm.tsx
@@ -1,215 +1,18 @@
 'use client'
 
-import { useMemo, useState } from 'react'
-import { toast } from 'sonner'
+import { useMemo } from 'react'
 import TableHeader from '../TableHeader'
 import useToggle from '@/hooks/useToggle'
-import {
-  DaysOfTheWeekEnum,
-  OpeningHoursData,
-  OpeningHoursDayData,
-  OpeningHoursFormProps
-} from '../types'
+import { OpeningHoursFormProps } from '../types'
 import DayRowEdit from './DayRowEdit'
 import CancelButton from './CancelButton'
 import EditButton from './EditButton'
 import UpdateButton from './UpdateButton'
-import { formatDateToTime, formatStringTimeIntoDate } from '@/lib/utils'
-import { updateOpeningHours } from '@/db/queries/openingHours.query'
+import { toast } from 'sonner'
+import onOpeningHoursSubmit from '@/actions/openingHoursAction'
 
-const OpeningHoursForm = ({
-  openingHoursData: incomingData
-}: OpeningHoursFormProps) => {
-  const [openingHoursData, setOpeningHoursData] = useState(incomingData)
-
+const OpeningHoursForm = ({ openingHoursData }: OpeningHoursFormProps) => {
   const [isEdit, handleToggleEdit] = useToggle(false)
-
-  const onSubmit = async (formData: FormData) => {
-    const mondayStart = formatStringTimeIntoDate(
-      String(formData?.get('mondayStart'))
-    )
-    const mondayEnd = formatStringTimeIntoDate(
-      String(formData?.get('mondayEnd'))
-    )
-
-    const tuesdayStart = formatStringTimeIntoDate(
-      String(formData?.get('tuesdayStart'))
-    )
-    const tuesdayEnd = formatStringTimeIntoDate(
-      String(formData?.get('tuesdayEnd'))
-    )
-
-    const wednesdayStart = formatStringTimeIntoDate(
-      String(formData?.get('wednesdayStart'))
-    )
-    const wednesdayEnd = formatStringTimeIntoDate(
-      String(formData?.get('wednesdayEnd'))
-    )
-
-    const thursdayStart = formatStringTimeIntoDate(
-      String(formData?.get('thursdayStart'))
-    )
-    const thursdayEnd = formatStringTimeIntoDate(
-      String(formData?.get('thursdayEnd'))
-    )
-
-    const fridayStart = formatStringTimeIntoDate(
-      String(formData?.get('fridayStart'))
-    )
-    const fridayEnd = formatStringTimeIntoDate(
-      String(formData?.get('fridayEnd'))
-    )
-
-    const saturdayStart = formatStringTimeIntoDate(
-      String(formData?.get('saturdayStart'))
-    )
-    const saturdayEnd = formatStringTimeIntoDate(
-      String(formData?.get('saturdayEnd'))
-    )
-
-    const sundayStart = formatStringTimeIntoDate(
-      String(formData?.get('sundayStart'))
-    )
-    const sundayEnd = formatStringTimeIntoDate(
-      String(formData?.get('sundayEnd'))
-    )
-
-    const openingHoursDayData: OpeningHoursDayData = {
-      mondayStart,
-      mondayEnd,
-      tuesdayStart,
-      tuesdayEnd,
-      wednesdayStart,
-      wednesdayEnd,
-      thursdayStart,
-      thursdayEnd,
-      fridayStart,
-      fridayEnd,
-      saturdayStart,
-      saturdayEnd,
-      sundayStart,
-      sundayEnd
-    }
-
-    try {
-      const jsonData = await updateOpeningHours({
-        id: 1,
-        body: openingHoursDayData
-      })
-
-      const {
-        mondayStart,
-        mondayEnd,
-        tuesdayStart,
-        tuesdayEnd,
-        wednesdayStart,
-        wednesdayEnd,
-        thursdayStart,
-        thursdayEnd,
-        fridayStart,
-        fridayEnd,
-        saturdayStart,
-        saturdayEnd,
-        sundayStart,
-        sundayEnd
-      } = jsonData
-
-      const openingHoursData: OpeningHoursData[] = [
-        {
-          day: DaysOfTheWeekEnum.Monday,
-          dayTranslation: 'Lundi',
-          inputStartName: 'mondayStart',
-          inputStartValue: formatDateToTime(mondayStart),
-          inputEndName: 'mondayEnd',
-          inputEndValue: formatDateToTime(mondayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Tuesday,
-          dayTranslation: 'Mardi',
-          inputStartName: 'tuesdayStart',
-          inputStartValue: formatDateToTime(tuesdayStart),
-          inputEndName: 'tuesdayEnd',
-          inputEndValue: formatDateToTime(tuesdayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Wednesday,
-          dayTranslation: 'Mercredi',
-          inputStartName: 'wednesdayStart',
-          inputStartValue: formatDateToTime(wednesdayStart),
-          inputEndName: 'wednesdayEnd',
-          inputEndValue: formatDateToTime(wednesdayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Thursday,
-          dayTranslation: 'Jeudi',
-          inputStartName: 'thursdayStart',
-          inputStartValue: formatDateToTime(thursdayStart),
-          inputEndName: 'thursdayEnd',
-          inputEndValue: formatDateToTime(thursdayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Friday,
-          dayTranslation: 'Vendredi',
-          inputStartName: 'fridayStart',
-          inputStartValue: formatDateToTime(fridayStart),
-          inputEndName: 'fridayEnd',
-          inputEndValue: formatDateToTime(fridayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Saturday,
-          dayTranslation: 'Samedi',
-          inputStartName: 'saturdayStart',
-          inputStartValue: formatDateToTime(saturdayStart),
-          inputEndName: 'saturdayEnd',
-          inputEndValue: formatDateToTime(saturdayEnd)
-        },
-        {
-          day: DaysOfTheWeekEnum.Sunday,
-          dayTranslation: 'Dimanche',
-          inputStartName: 'sundayStart',
-          inputStartValue: formatDateToTime(sundayStart),
-          inputEndName: 'sundayEnd',
-          inputEndValue: formatDateToTime(sundayEnd)
-        }
-      ]
-
-      if (openingHoursData) {
-        setOpeningHoursData(openingHoursData)
-        toast.success('Les données ont bien été mis à jour !')
-        handleToggleEdit()
-      }
-    } catch (error) {
-      toast.error(
-        `Attention les données n'ont pas pu être mis à jour. La raison : ${error}`
-      )
-    }
-  }
-
-  const openingHoursDataMemorized = useMemo(
-    () =>
-      openingHoursData?.map(
-        ({
-          day,
-          dayTranslation,
-          inputStartName,
-          inputStartValue,
-          inputEndName,
-          inputEndValue
-        }) => (
-          <DayRowEdit
-            key={dayTranslation}
-            day={day}
-            dayTranslation={dayTranslation}
-            inputStartName={inputStartName}
-            inputStartValue={inputStartValue}
-            inputEndName={inputEndName}
-            inputEndValue={inputEndValue}
-            isEdit={isEdit}
-          />
-        )
-      ),
-    [isEdit, openingHoursData]
-  )
 
   const editableSection = useMemo(
     () => (
@@ -227,12 +30,45 @@ const OpeningHoursForm = ({
 
   return (
     <form
-      action={async formData => await onSubmit(formData)}
+      action={async formData => {
+        const error = await onOpeningHoursSubmit(formData)
+
+        if (error) {
+          return toast.error(
+            `Attention les données n'ont pas pu être mis à jour. La raison : ${error}`
+          )
+        }
+
+        toast.success('Les données ont bien été mis à jour !')
+        handleToggleEdit()
+      }}
       className='h-full w-full'
     >
       <table className='w-full flex-grow' data-test='openingHours'>
         <TableHeader day={''} lunch={'Ouverture'} dinner={'Fermeture'} />
-        <tbody className='text-center'>{openingHoursDataMemorized}</tbody>
+        <tbody className='text-center'>
+          {openingHoursData?.map(
+            ({
+              day,
+              dayTranslation,
+              inputStartName,
+              inputStartValue,
+              inputEndName,
+              inputEndValue
+            }) => (
+              <DayRowEdit
+                key={dayTranslation}
+                day={day}
+                dayTranslation={dayTranslation}
+                inputStartName={inputStartName}
+                inputStartValue={inputStartValue}
+                inputEndName={inputEndName}
+                inputEndValue={inputEndValue}
+                isEdit={isEdit}
+              />
+            )
+          )}
+        </tbody>
       </table>
       {editableSection}
     </form>
