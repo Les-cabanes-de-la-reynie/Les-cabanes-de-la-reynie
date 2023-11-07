bad42a956606a7319968784a79d4cd230ea99745
davidbourrel <david-bourrel@hotmail.fr>
2023-11-07 21:38:43 +0100

    fixed

--- src/components/modules/Accommodations/AccommodationsSliderSkeleton.tsx
+++ src/components/modules/Accommodations/AccommodationsSliderSkeleton.tsx
@@ -2,14 +2,20 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 const AccommodationsSliderSkeleton = () => {
   return (
-    <Skeleton className='mb-5 bg-transparent'>
-      <Skeleton className='mb-4 h-[36px] bg-popover pl-4'></Skeleton>
-      <Skeleton className='grid w-full grid-flow-col grid-cols-1 gap-2 overflow-auto bg-transparent sm:grid-cols-2 lg:grid-cols-3'>
-        <Skeleton className='mx-1 h-60 bg-popover object-cover'></Skeleton>
-        <Skeleton className='mx-1 h-60 bg-popover object-cover'></Skeleton>
-        <Skeleton className='mx-1 h-60 bg-popover object-cover'></Skeleton>
-      </Skeleton>
-    </Skeleton>
+    <div className='mb-5'>
+      <div className='mb-2 flex items-center justify-between pl-4'>
+        <Skeleton className='h-[36px] w-32'></Skeleton>
+        <div className='flex'>
+          <Skeleton className='h-10 w-10 rounded-full'></Skeleton>
+          <Skeleton className='ml-2 h-10 w-10 rounded-full'></Skeleton>
+        </div>
+      </div>
+      <div className='grid w-full grid-flow-col grid-cols-1 gap-2 overflow-auto sm:grid-cols-2 lg:grid-cols-3'>
+        <Skeleton className='mx-1 h-60 object-cover'></Skeleton>
+        <Skeleton className='mx-1 h-60 object-cover'></Skeleton>
+        <Skeleton className='mx-1 h-60 object-cover'></Skeleton>
+      </div>
+    </div>
   )
 }
 export default AccommodationsSliderSkeleton
