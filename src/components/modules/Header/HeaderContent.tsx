aa18409f10883f6b113cbc4d7fd4c48e1811f6a8
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2023-11-14 16:06:53 +0100

    accommodations popover fixed (#39)

--- src/components/modules/Header/HeaderContent.tsx
+++ src/components/modules/Header/HeaderContent.tsx
@@ -14,7 +14,7 @@ const HeaderContent = ({
   return (
     <div
       className={cn(
-        'invisible fixed left-0 right-0 top-[4.5rem] box-border flex h-[calc(100vh-4.5rem)] w-full -translate-x-[100vw] flex-col overflow-auto bg-primary-dark px-4 py-2 transition-transform duration-300 md:px-6 md:py-4 lg:visible lg:static lg:h-full lg:w-auto lg:flex-grow lg:translate-x-0 lg:translate-y-0 lg:flex-row lg:items-center lg:justify-between lg:overflow-hidden lg:bg-inherit lg:px-0 lg:py-0 lg:duration-0',
+        'invisible fixed left-0 right-0 top-[4.5rem] box-border flex h-[calc(100vh-4.5rem)] w-full -translate-x-[100vw] flex-col bg-primary-dark px-4 py-2 transition-transform duration-300 md:px-6 md:py-4 lg:visible lg:static lg:h-full lg:w-auto lg:flex-grow lg:translate-x-0 lg:translate-y-0 lg:flex-row lg:items-center lg:justify-between lg:bg-inherit lg:px-0 lg:py-0 lg:duration-0',
         {
           'visible translate-x-0': isBurgerMenuOpen
         }
