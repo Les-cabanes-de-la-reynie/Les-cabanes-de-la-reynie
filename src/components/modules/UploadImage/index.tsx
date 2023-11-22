c4b095191833f721aace9f94aeb9f813ac2f1ef4
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2023-11-22 21:11:44 +0100

    Upload image protected and refactored (#50)

--- src/components/modules/UploadImage/index.tsx
+++ src/components/modules/UploadImage/index.tsx
@@ -3,9 +3,8 @@
 import Heading from '@/components/elements/Heading'
 import P from '@/components/elements/P'
 import { Separator } from '@/components/ui/separator'
-import { UploadDropzone } from '@/lib/uploadthing'
 import { useUser } from '@auth0/nextjs-auth0/client'
-import { toast } from 'sonner'
+import UploadImageCard from './UploadImageCard'
 
 const UploadImage = () => {
   const { user, error } = useUser()
@@ -15,7 +14,7 @@ const UploadImage = () => {
   }
 
   return (
-    <div>
+    <section>
       <Heading level={2} className='mt-8'>
         Upload image
       </Heading>
@@ -32,37 +31,13 @@ const UploadImage = () => {
           <Heading level={4} className='mt-2'>
             Header image
           </Heading>
-          <UploadDropzone
-            endpoint='yourteImages'
-            onClientUploadComplete={() => toast.success('Upload Completed')}
-            onUploadError={(error: Error) => {
-              toast.error(`ERROR! ${error.message}`)
-            }}
-            appearance={{
-              button:
-                'ut-uploading:cursor-not-allowed rounded-lg bg-primary-dark',
-              container:
-                'flex flex-col items-center justify-between rounded-lg border border-muted bg-popover'
-            }}
-          />
+          <UploadImageCard endpoint='yourteHeader' />
         </div>
         <div className='w-full'>
           <Heading level={4} className='mt-2'>
             Slider images
           </Heading>
-          <UploadDropzone
-            endpoint='yourteImages'
-            onClientUploadComplete={() => toast.success('Upload Completed')}
-            onUploadError={(error: Error) => {
-              toast.error(`ERROR! ${error.message}`)
-            }}
-            appearance={{
-              button:
-                'ut-uploading:cursor-not-allowed rounded-lg bg-primary-dark',
-              container:
-                'flex flex-col items-center justify-between rounded-lg border border-muted bg-popover'
-            }}
-          />
+          <UploadImageCard endpoint='yourteSlider' />
         </div>
       </div>
 
@@ -77,40 +52,16 @@ const UploadImage = () => {
           <Heading level={4} className='mt-2'>
             Header image
           </Heading>
-          <UploadDropzone
-            endpoint='cabaneImages'
-            onClientUploadComplete={() => toast.success('Upload Completed')}
-            onUploadError={(error: Error) => {
-              toast.error(`ERROR! ${error.message}`)
-            }}
-            appearance={{
-              button:
-                'ut-uploading:cursor-not-allowed rounded-lg bg-primary-dark',
-              container:
-                'flex flex-col items-center justify-between rounded-lg border border-muted bg-popover'
-            }}
-          />
+          <UploadImageCard endpoint='cabaneHeader' />
         </div>
         <div className='w-full'>
           <Heading level={4} className='mt-2'>
             Slider images
           </Heading>
-          <UploadDropzone
-            endpoint='cabaneImages'
-            onClientUploadComplete={() => toast.success('Upload Completed')}
-            onUploadError={(error: Error) => {
-              toast.error(`ERROR! ${error.message}`)
-            }}
-            appearance={{
-              button:
-                'ut-uploading:cursor-not-allowed rounded-lg bg-primary-dark',
-              container:
-                'flex flex-col items-center justify-between rounded-lg border border-muted bg-popover'
-            }}
-          />
+          <UploadImageCard endpoint='cabaneSlider' />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
 export default UploadImage
