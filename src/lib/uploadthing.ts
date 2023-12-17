9e3a9c407d6ecfbb981027b345027077bdb4022c
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2023-12-17 21:33:18 +0100

    revert last fix with old prisma client (#60)
    Co-authored-by: Les-cabanes-de-la-reynie <148213682+Les-cabanes-de-la-reynie@users.noreply.github.com>

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,6 @@
-import { OurFileRouter } from '@/app/api/uploadthing/core'
 import { generateComponents } from '@uploadthing/react'
+import { OurFileRouter } from '@/app/api/uploadthing/core'
+
 
 export const { UploadButton, UploadDropzone, Uploader } =
   generateComponents<OurFileRouter>()
