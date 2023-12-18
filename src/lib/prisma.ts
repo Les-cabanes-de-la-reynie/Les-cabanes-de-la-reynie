5c1823b91fc1c25ead2445c0696cdd854413d1d0
davidbourrel <david-bourrel@hotmail.fr>
2023-12-18 21:13:45 +0100

    prisma ts changed

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,17 @@
-// @ts-nocheck
-
 import { PrismaClient } from '@prisma/client'
 
-export const prisma: PrismaClient =
-  global.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error']
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
