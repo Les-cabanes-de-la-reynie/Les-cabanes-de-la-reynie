7c5ea8ce18fab1fc2fabb9efabb2dfc6a0844b23
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2023-12-30 22:04:18 +0100

    theme switcher e2e tests done (#78)

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL,
 
+    colorScheme: 'dark',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
   },
