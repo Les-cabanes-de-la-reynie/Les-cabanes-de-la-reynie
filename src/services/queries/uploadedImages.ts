dca689352a31afd73e147645de59fa1bcdb68386
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2024-01-01 19:42:57 +0100

    remove unstable_noStore to optimize next cache (#79)

--- src/services/queries/uploadedImages.ts
+++ src/services/queries/uploadedImages.ts
@@ -1,11 +1,9 @@
 import { UploadImageCategoryKeyEnum } from '@/_types/uploadImage'
 import { db } from '@/lib/prisma'
-import { unstable_noStore } from 'next/cache'
 
 export const getUploadedImages = async (
   categoryKey: UploadImageCategoryKeyEnum
 ) => {
-  unstable_noStore()
   return await db.image.findMany({
     where: {
       category: categoryKey
