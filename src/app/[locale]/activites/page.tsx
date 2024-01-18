26704fac2338ae5f56257953d945fa6505a50fe4
davidbourrel <david-bourrel@hotmail.fr>
2024-01-18 20:03:29 +0100

    remove useless translations in activities page

--- src/app/[locale]/activites/page.tsx
+++ src/app/[locale]/activites/page.tsx
@@ -1,4 +1,3 @@
-import { useTranslations } from 'next-intl'
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server'
 import { env } from '@/lib/env'
 import Heading from '@/components/elements/Heading'
@@ -22,14 +21,12 @@ export async function generateMetadata({
 const Activites = ({ params: { locale } }: { params: { locale: string } }) => {
   unstable_setRequestLocale(locale)
 
-  const t = useTranslations('SEO')
-
   return (
     <Container>
       <Heading level={1} className='my-8 text-center'>
         ACTIVITES
       </Heading>
-      <div className='mb-8 flex flex-grow flex-col'>{t('homeDescription')}</div>
+      <div className='mb-8 flex flex-grow flex-col'>Incoming</div>
     </Container>
   )
 }
