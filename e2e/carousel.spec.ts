0abb73be54fc7b2835a0c3247b9ec4da5d9bcf2f
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2024-02-09 21:16:52 +0100

    carousel e2e test added (#106)

---
+++ e2e/carousel.spec.ts
@@ -0,0 +1,49 @@
+import { expect, test } from '@playwright/test'
+import { MOBILE_VIEWPORT, frenchURL } from '../playwright.config'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(frenchURL)
+})
+
+test.describe('Carousel', () => {
+  test.describe('Mobile', () => {
+    // Change viewport in mobile size
+    test.use({ viewport: MOBILE_VIEWPORT })
+
+    test('should open and close image with fslightbox library', async ({
+      page
+    }) => {
+      await expect(page).toHaveURL(frenchURL)
+
+      const firstImage = page.getByRole('img', { name: 'Our fabulous place 0' })
+      const closeFslightboxButton = page.getByTitle('Close')
+      const fullscreenFslightboxButton = page.getByTitle('Enter fullscreen')
+
+      await expect(closeFslightboxButton).not.toBeVisible()
+      await expect(fullscreenFslightboxButton).not.toBeVisible()
+
+      await firstImage.click({ delay: 1000 })
+
+      await expect(closeFslightboxButton).toBeVisible()
+      await expect(fullscreenFslightboxButton).toBeVisible()
+    })
+
+    test('should switch image with previous and next button', async ({
+      page
+    }) => {
+      await expect(page).toHaveURL(frenchURL)
+
+      const firstImage = page.getByRole('img', { name: 'Our fabulous place 0' })
+      const nextButton = page.getByTestId('carouselNextButton')
+      const previousButton = page.getByTestId('carouselPreviousButton')
+
+      await expect(firstImage).toBeInViewport()
+
+      await nextButton.click({ delay: 2000, clickCount: 1 })
+      await expect(firstImage).not.toBeInViewport()
+
+      await previousButton.click({ delay: 2000, clickCount: 1 })
+      await expect(firstImage).toBeInViewport()
+    })
+  })
+})
