67a1832a9ca1a034045b05399b0760115b851860
davidbourrel <david-bourrel@hotmail.fr>
2024-02-11 18:57:57 +0100

    uploadthing update done

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,9 +1,9 @@
-import { createNextRouteHandler } from 'uploadthing/next'
-import { ourFileRouter } from './core'
 import { env } from '@/lib/env'
+import { createRouteHandler } from 'uploadthing/next'
+import { ourFileRouter } from './core'
 
 // Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
   config: {
     uploadthingId: env.UPLOADTHING_APP_ID,
