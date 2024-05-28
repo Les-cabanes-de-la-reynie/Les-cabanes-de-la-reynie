f57456a46ef422416e8dfe21699e36d550ed41f2
davidbourrel <david-bourrel@hotmail.fr>
2024-05-28 21:57:22 +0200

    text in OurGourmetOfferStrong component fixed

--- src/components/modules/OurGourmetOffer/OurGourmetOfferStrong.tsx
+++ src/components/modules/OurGourmetOffer/OurGourmetOfferStrong.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 const OurGourmetOfferStrong = ({ children }: PropsWithChildren) => {
   return (
-    <strong className='before:block before:absolute before:inset-0 before:-skew-y-1 before:bg-primary relative inline-block'>
+    <strong className='before:block before:absolute before:inset-0 before:-skew-y-1 before:bg-primary relative inline-block text-pretty mr-1'>
       <span className='relative text-white'>{children}</span>
     </strong>
   )
