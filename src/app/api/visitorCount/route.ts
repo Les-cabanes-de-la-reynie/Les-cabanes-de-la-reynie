60c1260065c6c9f6bb34b9f6a176bfae06b5cbcb
David Bourrel <64352100+davidbourrel@users.noreply.github.com>
2024-10-14 16:54:16 +0200

    Visitor counter feature added (#161)
    * Visitor counter feature added
    
    * algo more secure

---
+++ src/app/api/visitorCount/route.ts
@@ -0,0 +1,56 @@
+import { db } from '@/lib/prisma'
+import { getVisitorCount } from '@/services/queries/visitorCount'
+import { isSameDay } from 'date-fns'
+import { NextResponse } from 'next/server'
+
+export const POST = async (request: Request) => {
+  try {
+    const lastVisitDate = await request.json()
+
+    if (!lastVisitDate) {
+      // First visit
+      const visitorCount = await getVisitorCount()
+
+      await db.visitorCount.update({
+        where: { id: 1 },
+        data: { count: visitorCount.count + 1 }
+      })
+
+      return new NextResponse(
+        JSON.stringify({ message: 'Visitor count incremented' }),
+        { status: 200 }
+      )
+    }
+
+    const isVisitedToday = isSameDay(Number(lastVisitDate), Date.now())
+
+    if (!isVisitedToday) {
+      const visitorCount = await getVisitorCount()
+
+      await db.visitorCount.update({
+        where: { id: 1 },
+        data: { count: visitorCount.count + 1 }
+      })
+
+      return new NextResponse(
+        JSON.stringify({ message: 'Visitor count incremented' }),
+        { status: 200 }
+      )
+    }
+
+    // Same day so do nothing
+    return new NextResponse(
+      JSON.stringify({ message: 'Not visitor count incrementation needed' }),
+      { status: 200 }
+    )
+
+    // eslint-disable-next-line
+  } catch (error: any) {
+    return new NextResponse(
+      'Error in incrementing visitor count' + error.message,
+      {
+        status: 500
+      }
+    )
+  }
+}
---
+++ src/hooks/VisitorCount.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { LAST_VISIT_KEY } from '@/_constants/visitorCount'
+import { env } from '@/lib/env'
+import { isSameDay } from 'date-fns'
+import { useEffect } from 'react'
+
+const incrementVisitor = async (lastVisitDate: string | null) => {
+  const url = `${env.NEXT_PUBLIC_BASE_URL}/api/visitorCount`
+
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(lastVisitDate)
+  })
+}
+
+const VisitorCount = () => {
+  useEffect(() => {
+    const lastVisitDate = localStorage.getItem(LAST_VISIT_KEY)
+
+    if (!lastVisitDate) {
+      // First visit
+      const newDate = Date.now().toString()
+
+      incrementVisitor(lastVisitDate)
+      return localStorage.setItem(LAST_VISIT_KEY, newDate)
+    }
+
+    const isVisitedToday = isSameDay(Number(lastVisitDate), Date.now())
+
+    if (!isVisitedToday) {
+      const newDate = Date.now().toString()
+
+      incrementVisitor(lastVisitDate)
+      return localStorage.setItem(LAST_VISIT_KEY, newDate)
+    }
+  }, [])
+
+  return <></>
+}
+export default VisitorCount
