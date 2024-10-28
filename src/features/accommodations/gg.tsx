bf765860d3d5fe88cb4e1a8ed86c8f659f0fc6fe
davidbourrel <david-bourrel@hotmail.fr>
2024-10-28 13:45:44 +0100

    a

--- src/features/accommodations/gg.tsx
+++ src/features/accommodations/gg.tsx
@@ -1,53 +1,27 @@
-'use client'
+import { P } from '@/components/P'
+import { getHutData } from '@/services/queries/hut'
+import { getYurtData } from '@/services/queries/yurt'
+import { unstable_noStore } from 'next/cache'
+import { AccommodationTypeEnum } from './types'
 
-import { PopoverCloseButton } from '@/components/PopoverCloseButton'
-import { Button } from '@/components/ui/button'
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger
-} from '@/components/ui/popover'
-import { Separator } from '@/components/ui/separator'
-import { useTranslations } from 'next-intl'
-import Link from 'next/link'
-import { BookEntity } from './types'
-
-type AccommodationsPopoverProps = {
-  bookList: BookEntity[]
+type AccommodationsPriceProps = {
+  accommodationType: AccommodationTypeEnum
+  description: string
 }
 
-export const AccommodationsPopover = ({
-  bookList
-}: AccommodationsPopoverProps) => {
-  const t = useTranslations('Accommodations')
-  const t2 = useTranslations('Common')
+export const AccommodationsPrice = async ({
+  accommodationType,
+  description
+}: AccommodationsPriceProps) => {
+  unstable_noStore()
+  const accommodationPrice =
+    accommodationType === AccommodationTypeEnum.HUT
+      ? (await getHutData()).price
+      : (await getYurtData()).price
 
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button className='mt-10 lg:w-max'>
-          {t2('bookYourStay')}
-          <span className='sr-only'>Book list</span>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <>
-          <span className='mb-2 select-none'>{t('bookList')}</span>
-          <Separator className='mb-2' />
-          <ul className='flex flex-col gap-2'>
-            {bookList?.map(({ title, href }) => (
-              <li key={`${title}-${href}`}>
-                <Button asChild>
-                  <Link href={href} target='_blank' rel='noopener noreferrer'>
-                    {title}
-                  </Link>
-                </Button>
-              </li>
-            ))}
-          </ul>
-        </>
-        <PopoverCloseButton />
-      </PopoverContent>
-    </Popover>
+    <P className='text-foreground'>
+      {description} <span className='font-bold'>{accommodationPrice}€</span>
+    </P>
   )
 }
