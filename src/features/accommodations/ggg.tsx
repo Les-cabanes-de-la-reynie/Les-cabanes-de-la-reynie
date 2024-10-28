7f282595a6046ba545ebc2a44792746ef9fa4dae
davidbourrel <david-bourrel@hotmail.fr>
2024-10-28 13:50:16 +0100

    b

--- src/features/accommodations/ggg.tsx
+++ src/features/accommodations/ggg.tsx
@@ -1,46 +1,18 @@
+import { Container } from '@/components/Container'
 import { Heading } from '@/components/Heading'
-import Image, { StaticImageData } from 'next/image'
-import Link from 'next/link'
+import { P } from '@/components/P'
+import { useTranslations } from 'next-intl'
 
-type AccommodationsCardProps = {
-  href: string
-  imageOnFront: StaticImageData
-  imageOnHover: StaticImageData
-  textContent: string
-}
+export const AccommodationsDescription = () => {
+  const t = useTranslations('Home')
 
-export const AccommodationsCard = ({
-  href,
-  imageOnFront,
-  imageOnHover,
-  textContent
-}: AccommodationsCardProps) => {
   return (
-    <div className='group rounded-lg border-4 w-full h-60 md:h-full md:max-w-max border-transparent hover:border-primary duration-500 transition-colors'>
-      <Link href={href}>
-        <div className='relative h-full'>
-          <Image
-            src={imageOnFront}
-            className='h-full w-full rounded object-cover group-hover:opacity-0 transition-opacity duration-500 ease-in-out'
-            alt='TODO'
-            width={384}
-            height={384}
-          />
-          <Image
-            src={imageOnHover}
-            className='h-full w-full rounded opacity-0 group-hover:opacity-100 object-cover absolute top-0 left-0 transition-opacity duration-500 ease-in-out'
-            alt='TODO'
-            width={384}
-            height={384}
-          />
-          <Heading
-            level={2}
-            className='flex items-center rounded text-primary-foreground justify-center h-full w-full absolute top-0 left-0 bg-opacity-50'
-          >
-            {textContent}
-          </Heading>
-        </div>
-      </Link>
-    </div>
+    <Container>
+      <div className=' w-full'>
+        <Heading level={3}>{t('accommodationsTitle')}</Heading>
+        <P>{t('accommodationsP1')}</P>
+        <P>{t('accommodationsP2')}</P>
+      </div>
+    </Container>
   )
 }
