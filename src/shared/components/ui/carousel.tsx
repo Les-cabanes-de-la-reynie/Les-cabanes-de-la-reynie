b4d7c41bfce4d15a15accd01386d02f29fef7f0f
davidbourrel <david-bourrel@hotmail.fr>
2025-08-24 20:36:55 +0200

    fix build

--- src/shared/components/ui/carousel.tsx
+++ src/shared/components/ui/carousel.tsx
@@ -178,7 +178,7 @@ function CarouselPrevious({
   size = 'icon',
   ...props
 }: React.ComponentProps<typeof Button>) {
-  const { orientation, scrollPrev, canScrollPrev } = useCarousel()
+  const { scrollPrev, canScrollPrev } = useCarousel()
 
   return (
     <Button
@@ -206,7 +206,7 @@ function CarouselNext({
   size = 'icon',
   ...props
 }: React.ComponentProps<typeof Button>) {
-  const { orientation, scrollNext, canScrollNext } = useCarousel()
+  const { scrollNext, canScrollNext } = useCarousel()
 
   return (
     <Button
